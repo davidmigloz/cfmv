2f8e6c5bf8d3816340b5a83ac72001f9780d1df8
davidmigloz <me@davidmiguel.com>
2015-11-11 21:21:01 +0100

    Create maven project

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $com.davidmiguel.cfmv;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
