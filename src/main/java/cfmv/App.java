f5e63704e29d474a118de4f5a7e348eb5c272e92
davidmigloz <me@davidmiguel.com>
2015-11-12 21:57:10 +0100

    Minor changes in App.java

--- src/main/java/cfmv/App.java
+++ src/main/java/cfmv/App.java
@@ -8,51 +8,61 @@ import java.util.List;
 import au.com.bytecode.opencsv.CSVReader;
 
 public class App {
-	private String data = "src/main/resources/data.csv";
-	private List<Point> points;
-
-	public App(String data) {
-		this.data = data;
-		this.points = new ArrayList<Point>();
-	}
-
-	private void parseCSV() {
-		try {
-			CSVReader reader = new CSVReader(new FileReader(data), ';', '"', 1);
-
-			String[] nextLine;
-			long line = 0;
-			while ((nextLine = reader.readNext()) != null) {
-				int[] features = new int[nextLine.length];
-				for(int i = 0; i < nextLine.length; i++){
-					if(nextLine[i].equals("")){
-						System.out.println("Missed value!");
-						features[i] = 0;
-					} else {
-						features[i] = Integer.parseInt(nextLine[i]);
-					}
-				}
-				Point p = new Point(line, features);
-				points.add(p);
-				line++;
-			}
-			reader.close();
-		} catch (IOException e) {
-			System.out.println("Error I/O: " + e.getMessage());
-		} catch (NumberFormatException e){
-			System.out.println("Wrong value. " + e.getMessage());
-		}
-	}
-	
-	private void show(){
-		for(Point p : points){
-			System.out.println(p);
-		}
-	}
 
 	public static void main(String[] args) {
-		App myApp = new App("src/main/resources/data.csv");
-		myApp.parseCSV();
-		myApp.show();
+		String dataSet = "src/main/resources/data.csv";
+
+		try {
+			List<Point> puntos = processData(dataSet);
+
+		} catch (IOException e) {
+			System.out
+					.println("Error at processing data set. " + e.getMessage());
+		} catch (NumberFormatException e) {
+			System.out.println("Wrong value. " + e.getMessage());
+		}
+
+	}
+
+	/**
+	 * Parses the data set and creates Point objetcs with the features given.
+	 * 
+	 * @param dataSet
+	 *            data set in CSV format
+	 * @return list of Point objects contains in the data set
+	 * @throws NumberFormatException
+	 * @throws IOException
+	 */
+	private static List<Point> processData(String dataSet)
+			throws NumberFormatException, IOException {
+		List<Point> points; // List with Point objetcs obtained after parsing
+		String[] point; // Array of strings with the features of a point
+		long num = 0; // Number of points processed
+		final char SEPARATOR = ';'; // Separator of the values of the CSV file
+		final char ESCAPE_CHAR = '"'; // Escape character in the CSV file
+		final int FIRST_LINE = 1; // First line to read (starting from 0)
+
+		// Set up CSV reader
+		CSVReader reader = new CSVReader(new FileReader(dataSet), SEPARATOR,
+				ESCAPE_CHAR, FIRST_LINE);
+		points = new ArrayList<Point>();
+
+		// Parse CSV dataset
+		while ((point = reader.readNext()) != null) {
+			int[] features = new int[point.length];
+			for (int i = 0; i < point.length; i++) {
+				if (point[i].equals("")) {
+					System.out.println("Missed value!");
+					features[i] = 0;
+				} else {
+					features[i] = Integer.parseInt(point[i]);
+				}
+			}
+			Point p = new Point(num, features);
+			points.add(p);
+			num++;
+		}
+		reader.close();
+		return points;
 	}
 }
